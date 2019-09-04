e622ef3beaff3d363142f44c258e75aab222f5e9
germangelv <germangelv@gmail.com>
2019-09-04 14:33:19 -0300

    Added RecyclerView functionality in News

---
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/news/NewsItem.kt
@@ -0,0 +1,30 @@
+package ar.com.wolox.android.example.ui.home.news
+
+class NewsItem(
+    private val mImageResource: Int,
+    private val mTitleResource: String,
+    private val mTextInformationResource: String,
+    private val mTimeResource: String,
+    private val mEmotionImage: Int
+) {
+
+    fun getImageResource(): Int {
+        return mImageResource
+    }
+
+    fun getTitleResource(): String {
+        return mTitleResource
+    }
+
+    fun getTextInformationResource(): String {
+        return mTextInformationResource
+    }
+
+    fun getTimeResource(): String {
+        return mTimeResource
+    }
+
+    fun getEmotionImage(): Int {
+        return mEmotionImage
+    }
+}
