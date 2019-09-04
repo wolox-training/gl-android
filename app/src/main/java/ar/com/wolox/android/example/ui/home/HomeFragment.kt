47cf28717ba0836554355b7dede35ae9507565a8
germangelv <germangelv@gmail.com>
2019-09-04 14:09:15 -0300

    Clean spaces code

--- app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package ar.com.wolox.android.example.ui.home
 
+import androidx.core.util.Pair
 import ar.com.wolox.android.R
 import ar.com.wolox.android.example.ui.home.news.NewsFragment
 import ar.com.wolox.android.example.ui.home.profile.ProfileFragment
@@ -7,13 +8,14 @@ import ar.com.wolox.wolmo.core.adapter.viewpager.SimpleFragmentPagerAdapter
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
 import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_home.*
-import androidx.core.util.Pair
 import javax.inject.Inject
 
 class HomeFragment : WolmoFragment<HomePresenter>(), HomeView {
 
-    @Inject internal lateinit var mPageNews: NewsFragment
-    @Inject internal lateinit var mPageProfile: ProfileFragment
+    @Inject
+    internal lateinit var mPageNews: NewsFragment
+    @Inject
+    internal lateinit var mPageProfile: ProfileFragment
     private lateinit var mFragmentHomePagerAdapter: SimpleFragmentPagerAdapter
 
     override fun layout(): Int {
@@ -25,8 +27,8 @@ class HomeFragment : WolmoFragment<HomePresenter>(), HomeView {
 
         mFragmentHomePagerAdapter = SimpleFragmentPagerAdapter(childFragmentManager).apply {
             addFragments(
-                    Pair(mPageNews, "News"),
-                    Pair(mPageProfile, "Profile")
+                    Pair(mPageProfile, "Profile"),
+                    Pair(mPageNews, "News")
             )
         }
 
@@ -36,7 +38,6 @@ class HomeFragment : WolmoFragment<HomePresenter>(), HomeView {
                     TabLayout.TabLayoutOnPageChangeListener(vTabFragmentHome)
             )
         }
-
         onTabInit()
     }
 
